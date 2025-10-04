7f424692077f40945de26034e31ec7a8d87986a4
Rafael GPL <rafael.gpl@dashws.com>
2025-10-04 11:12:25 -0700

    Initial Commit

---
+++ CameraPlus/CameraPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPlusApp.swift
+//  CameraPlus
+//
+//  Created by Rafael GPL on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
